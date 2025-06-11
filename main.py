fdf21d983d2f10d410bb26b555abb043202523b3
Antony-Rya <159479364+Antony-Rya@users.noreply.github.com>
2025-06-11 23:55:48 +0000

    v1.8(Menu principal, bugado aparentemente)

--- main.py
+++ main.py
@@ -23,4 +23,33 @@ ranking = Ranking(r)
 tarefas = Tarefa(r)
 usuarios = UsuariosOnline(r)
 ratelimiter = RateLimiter(r)
-ratelimitermenu(ratelimiter)
+
+
+
+def menuprincipal():
+    menu = True
+    while menu:
+        print('#########################')
+        print('1 - tarefas')
+        print('2 - contador')
+        print('3 - usuarios online')
+        print('4 - limitador de acesso')
+        print('5 - ranking')
+        print('#########################')
+        opcao = input('Digite sua opção... ')
+        match opcao:
+            case '1':
+                tarefamenu(tarefas)
+            case '2':
+                contadormenu(contador)
+            case '3':
+                usuariosmenu(usuarios)
+            case '4':
+                ratelimitermenu(ratelimiter)
+            case '5':
+                rankingmenu(ranking)
+            case _:
+                print('Opção inválida.')
+                limpar_terminal_com_atraso()
+
+menuprincipal()
